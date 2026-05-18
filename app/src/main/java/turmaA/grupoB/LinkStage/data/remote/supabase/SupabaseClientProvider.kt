773a545a4c2afa2795b593c26c81e57e167651dc
Tiago Rodrigues <tiagoalexandre@ipvc.pt>
2026-05-18 22:17:30 +0100

    feat(data): configure Supabase client

--- app/src/main/java/turmaA/grupoB/LinkStage/data/remote/supabase/SupabaseClientProvider.kt
+++ app/src/main/java/turmaA/grupoB/LinkStage/data/remote/supabase/SupabaseClientProvider.kt
@@ -1,4 +1,21 @@
 package turmaA.grupoB.LinkStage.data.remote.supabase
 
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.realtime.Realtime
+import io.github.jan.supabase.storage.Storage
+import turmaA.grupoB.LinkStage.BuildConfig
+
 object SupabaseClientProvider {
+
+    val client = createSupabaseClient(
+        supabaseUrl = BuildConfig.SUPABASE_URL,
+        supabaseKey = BuildConfig.SUPABASE_ANON_KEY
+    ) {
+        install(Auth)
+        install(Postgrest)
+        install(Storage)
+        install(Realtime)
+    }
 }
