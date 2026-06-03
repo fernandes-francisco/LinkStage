dc4d35f654a8891bd498e46eb480cd34a5aa3c77
Tiago Rodrigues <tiagoalexandre@ipvc.pt>
2026-06-03 10:24:51 +0100

    refactor(institution): add institution repository interface

--- app/src/main/java/turmaA/grupoB/LinkStage/data/repository/InstitutionRepository.kt
+++ app/src/main/java/turmaA/grupoB/LinkStage/data/repository/InstitutionRepository.kt
@@ -4,17 +4,17 @@ import io.github.jan.supabase.postgrest.from
 import turmaA.grupoB.LinkStage.data.remote.model.institution.InstitutionModel
 import turmaA.grupoB.LinkStage.data.remote.supabase.SupabaseClientProvider
 
-class InstitutionRepository {
+class InstitutionRepository : InstitutionRepositoryInterface{
     private val supabase = SupabaseClientProvider.client
 
-    suspend fun getInstitutions(): List<InstitutionModel>? {
+    override suspend fun getInstitutions(): List<InstitutionModel> {
         return supabase
             .from("institutions")
             .select()
             .decodeList<InstitutionModel>()
     }
 
-    suspend fun getInstitutionById(institutionId: String): InstitutionModel? {
+    override suspend fun getInstitutionById(institutionId: String): InstitutionModel? {
         return supabase
             .from("institutions")
             .select {
@@ -26,7 +26,7 @@ class InstitutionRepository {
             .firstOrNull()
     }
 
-    suspend fun getInstitutionByUserId(userId: String): InstitutionModel? {
+    override suspend fun getInstitutionByUserId(userId: String): InstitutionModel? {
         return supabase
             .from("institutions")
             .select {
