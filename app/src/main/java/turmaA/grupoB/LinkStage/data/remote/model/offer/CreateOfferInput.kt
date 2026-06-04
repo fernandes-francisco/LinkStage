fdba43a25d4c7a19740bdafb067ba22598abfd8e
Tiago Rodrigues <tiagoalexandre@ipvc.pt>
2026-06-04 18:12:30 +0100

    fix(offer): make create offer location optional

--- app/src/main/java/turmaA/grupoB/LinkStage/data/remote/model/offer/CreateOfferInput.kt
+++ app/src/main/java/turmaA/grupoB/LinkStage/data/remote/model/offer/CreateOfferInput.kt
@@ -12,7 +12,7 @@ data class CreateOfferInput(
     val title: String,
     val description: String,
     val area: String,
-    val location: String,
+    val location: String? = null,
     val salary: Double? = null,
     val modality: String? = null,
     val vacancies: Int = 1,
