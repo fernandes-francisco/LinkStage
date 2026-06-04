0b50412f26a6c831983d967eef2baed46aaa7da3
Tiago Rodrigues <tiagoalexandre@ipvc.pt>
2026-06-04 23:33:28 +0100

    feat(internship): expose internship creation

--- app/src/main/java/turmaA/grupoB/LinkStage/data/repository/InternshipRepositoryInterface.kt
+++ app/src/main/java/turmaA/grupoB/LinkStage/data/repository/InternshipRepositoryInterface.kt
@@ -4,6 +4,7 @@ import turmaA.grupoB.LinkStage.data.remote.model.enums.InternshipStatus
 import turmaA.grupoB.LinkStage.data.remote.model.internship.ActivityLogModel
 import turmaA.grupoB.LinkStage.data.remote.model.internship.AssignSupervisorInput
 import turmaA.grupoB.LinkStage.data.remote.model.internship.CreateActivityLogInput
+import turmaA.grupoB.LinkStage.data.remote.model.internship.CreateInternshipInput
 import turmaA.grupoB.LinkStage.data.remote.model.internship.InternshipModel
 
 interface InternshipRepositoryInterface {
@@ -12,6 +13,7 @@ interface InternshipRepositoryInterface {
     suspend fun getInternshipsByStudent(studentId: String): List<InternshipModel>
     suspend fun getInternshipsByInstitution(institutionId: String): List<InternshipModel>
     suspend fun getInternshipsByStatus(status: InternshipStatus): List<InternshipModel>
+    suspend fun createInternship(input: CreateInternshipInput): InternshipModel
     suspend fun assignSupervisor(internshipId: String, input: AssignSupervisorInput): InternshipModel
     suspend fun getActivityLogsByInternship(internshipId: String): List<ActivityLogModel>
     suspend fun createActivityLog(input: CreateActivityLogInput): ActivityLogModel
