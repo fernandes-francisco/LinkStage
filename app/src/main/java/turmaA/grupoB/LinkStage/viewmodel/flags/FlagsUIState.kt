865b658315923908aaa3141578026403009ed6ff
fernandesfrancisco <fernandesfrancisco@ipvc.pt>
2026-06-10 20:48:02 +0100

    fix: change FlagUiState return type from Imgs to List<Imgs>

--- app/src/main/java/turmaA/grupoB/LinkStage/viewmodel/flags/FlagsUIState.kt
+++ app/src/main/java/turmaA/grupoB/LinkStage/viewmodel/flags/FlagsUIState.kt
@@ -5,7 +5,7 @@ import turmaA.grupoB.LinkStage.data.remote.model.Imgs
 sealed class FlagsUIState {
     data object Idle: FlagsUIState()
     data object Loading: FlagsUIState()
-    data class Success(val imgs: Imgs) : FlagsUIState()
+    data class Success(val imgs: List<Imgs>) : FlagsUIState()
     data object Empty: FlagsUIState()
     data class Error( val message: String): FlagsUIState()
 }
