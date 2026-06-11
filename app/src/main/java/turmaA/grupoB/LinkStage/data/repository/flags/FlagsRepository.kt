904adbecda0964ecdaf3f07ee8668a3eb2d13e9b
fernandesfrancisco <fernandesfrancisco@ipvc.pt>
2026-06-11 19:43:48 +0100

    Revert "Load Portugal and GB flags from stable URLs"
    This reverts commit c4f9600c8875b42d8c875d447846644f174ead2a.

--- app/src/main/java/turmaA/grupoB/LinkStage/data/repository/flags/FlagsRepository.kt
+++ app/src/main/java/turmaA/grupoB/LinkStage/data/repository/flags/FlagsRepository.kt
@@ -4,15 +4,7 @@ import turmaA.grupoB.LinkStage.data.remote.RetrofitInstance
 import turmaA.grupoB.LinkStage.data.remote.model.Imgs
 
 class FlagsRepository : FlagsRepositoryInterface {
-    private val directFlagUrls = mapOf(
-        "portugal" to "https://flagcdn.com/w320/pt.png",
-        "gb" to "https://flagcdn.com/w320/gb.png",
-    )
-
     override suspend fun getFlag(country: String): Imgs? {
-        val normalizedCountry = country.lowercase()
-        directFlagUrls[normalizedCountry]?.let { return Imgs(png = it) }
-
-        return RetrofitInstance.api.getFlagByName(normalizedCountry).firstOrNull()?.flags
+        return RetrofitInstance.api.getFlagByName(country).firstOrNull()?.flags
     }
 }
