53894ecf62985ecdf2f8974a8ec365c9d7b6c500
fernandesfrancisco <fernandesfrancisco@ipvc.pt>
2026-06-11 17:03:09 +0100

    Route successful login to student home

--- app/src/main/java/turmaA/grupoB/LinkStage/ui/navigation/AppNavigation.kt
+++ app/src/main/java/turmaA/grupoB/LinkStage/ui/navigation/AppNavigation.kt
@@ -105,7 +105,7 @@ fun AppNavigation(
                             popUpTo(Routes.LOGIN) { inclusive = true }
                         }
                     } else {
-                        navController.navigate(Routes.ORIENTADOR_MAIN) {
+                        navController.navigate(Routes.ALUNO_MAIN) {
                             popUpTo(Routes.LOGIN) { inclusive = true }
                         }
                     }
