858ea721317ec9120f5e0970ecbb79cf84a48f43
MiguelAzevedo57 <miguelazevedo@ipvc.pt>
2026-06-11 11:21:32 +0100

    Teste

--- app/src/main/java/turmaA/grupoB/LinkStage/ui/navigation/AppNavigation.kt
+++ app/src/main/java/turmaA/grupoB/LinkStage/ui/navigation/AppNavigation.kt
@@ -105,7 +105,7 @@ fun AppNavigation(
                             popUpTo(Routes.LOGIN) { inclusive = true }
                         }
                     } else {
-                        navController.navigate(Routes.INSTITUICAO_MAIN) {
+                        navController.navigate(Routes.ORIENTADOR_MAIN) {
                             popUpTo(Routes.LOGIN) { inclusive = true }
                         }
                     }
